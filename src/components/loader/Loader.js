5fa3c7f6ad8bf3c82ecfc5e238b9d35a8a4640f2
Madhur Mata <you@example.com>
2022-06-04 10:56:46 +0800

    loader deleted

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Loader() {
   return (
-    <div className="spinner-container">
-      <div className="spinner"></div>
+    <div>
+      <div>Loading</div>
     </div>
   );
 }
