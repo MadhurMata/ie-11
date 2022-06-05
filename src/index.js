ec622c3a34180757b62aa576a1e41a5bd67a642a
Madhur Mata <you@example.com>
2022-06-05 11:31:13 +0800

    an other try-3

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
-import polyfillLibrary from 'polyfill-library';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
-polyfillLibrary.getPolyfillString({
-	uaString: 'Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)',
-	minify: true,
-	features: {
-		'es6': { flags: ['gated'] }
-	}
-}).then(function(bundleString) {
-	console.log(bundleString);
-});
 
 
 ReactDOM.render(
