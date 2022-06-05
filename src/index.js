71dd5ef14ddfc198236eb9c5bdde5a6a04e48946
Madhur Mata <you@example.com>
2022-06-05 11:03:26 +0800

    an other try

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
+const polyfillLibrary = require('polyfill-library');
+
+const polyfillBundle = polyfillLibrary.getPolyfillString({
+	uaString: 'Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)',
+	minify: true,
+	features: {
+		'es6': { flags: ['gated'] }
+	}
+}).then(function(bundleString) {
+	console.log(bundleString);
+});
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
 
 ReactDOM.render(
   <React.StrictMode>
